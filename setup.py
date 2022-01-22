1bb08da42142c7f51086ed30cc6ce0da54302ee8
Pan Dango <hoopandango@gmail.com>
2022-01-22 14:13:27 +0530

    testing for heroku part 2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='BC_Extractor',
+	version='1.0.0',
+	packages=[''],
+	url='https://github.com/hoopandango/BC_Extractor',
+	license='GPL',
+	author='Pan Dango',
+	author_email='hoopandango@gmail.com',
+	description='Battle cat funny stuff'
+)
